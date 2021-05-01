f1581362bc5dfb991a2c0b85d36ad1f10237a1de
tyanotyrano <tyrano09@gmail.com>
2021-05-01 19:48:55 +0900

    refactor(CarNameScanner.java): 잘못된 자동차 이름 입력 시 예외처리 추가
    잘못된 자동차 이름 입력 시, 올바른 값을 받을 때까지 다시 입력받는다.
    - CarNameScanner.java 의 scanCarNames() 호출 시, 재귀 방식으로 예외 처리 추가
    - CarNameScanner.java 의 scanCarNames() 호출 시, String -> CarName 객체로 포장하여 반환하도록 수정

--- src/main/java/racingcar/inputscan/CarNameScanner.java
+++ src/main/java/racingcar/inputscan/CarNameScanner.java
@@ -1,15 +1,38 @@
 package racingcar.inputscan;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import racingcar.CarName;
+import utils.ErrorMessage;
 import utils.inputscan.InputScanner;
 
 public class CarNameScanner {
 	private static final InputScanner INPUT_SCANNER = new InputScanner();
 	private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-	public List<String> scanCarNames() {
+	public List<CarName> scanCarNames() {
 		System.out.println(CAR_NAME_INPUT_MESSAGE);
-		return INPUT_SCANNER.scanStringSplitByComma();
+
+		try {
+			return mapToCarNames(INPUT_SCANNER.scanStringSplitByComma());
+		} catch (Exception e) {
+			resetForRescanCarNames();
+			return scanCarNames();
+		}
+	}
+
+	private List<CarName> mapToCarNames(List<String> inputNames) {
+		List<CarName> carNames = new ArrayList<>();
+
+		for (String carName : inputNames) {
+			carNames.add(new CarName(carName));
+		}
+
+		return carNames;
+	}
+
+	private void resetForRescanCarNames() {
+		System.out.println(ErrorMessage.CAR_NAME_LENGTH_MUST_HAVE_1_OR_MORE_AND_5_OR_LESS);
 	}
 }
